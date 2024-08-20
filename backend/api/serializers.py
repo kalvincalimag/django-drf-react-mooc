d40a0e71ac860d23056d4dd69d7f05f89e434e5b
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-08-20 14:07:50 +0800

    Configured JWT settings and added serializers for CustomUser and Profile models.

---
+++ backend/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from userauths.models import CustomUser, Profile
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
