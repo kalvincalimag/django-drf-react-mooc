c7b33168f6b8db06a95b1f595db1323addc72a03
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-08-21 09:36:50 +0800

    Extended JWT token serializer to include user data.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 from userauths.models import CustomUser, Profile
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+
+        return token 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
---
+++ backend/api/views.py
@@ -0,0 +1,5 @@
+from api.serializers import MyTokenObtainPairSerializer, CustomUserSerializer, ProfileSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
