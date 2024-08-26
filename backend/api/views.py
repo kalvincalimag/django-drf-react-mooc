7e6bfaf92317ccd53dbcc2a50edcc53d22a9f7f5
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-08-27 01:36:46 +0800

    Added OTP generation and refresh token handling for reset PW flow.

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,3 +14,29 @@ class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+    
+def generate_random_otp(length=7):
+    digits = "0123456789"
+    otp = ''.join(random.choice(digits) for _ in range(length))
+    return otp
+    
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = CustomUser.objects.filter(email=email).first()
+        
+        if user:            
+            uuidb64 = user.pk
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh.access_token)
+            user.refresh_token = refresh_token
+            user.otp = generate_random_otp()
+            user.save()
+            
+            link = f'http://localhost://5173/create-new-password/?otp={user.otp}&uuidb64={uuidb64}&refresh_token={refresh_token}'
+            print("link====", link)
+            
+        return user 
