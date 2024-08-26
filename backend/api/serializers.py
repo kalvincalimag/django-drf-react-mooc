b98deb079599aa5d6e8e5cad83569cb4426e543f
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-08-26 17:45:49 +0800

    Implement RegisterSerializer & View.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from userauths.models import CustomUser, Profile
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -14,6 +14,32 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         return token 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta: 
+        model = CustomUser
+        fields = ['full_name', 'email', 'password', 'password2']
+
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match. Please try again."})
+        return attr
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            full_name=validated_data['full_name'], 
+            email=validated_data['email'],
+        )
+        
+        email_username, _ = user.email.split("@")
+        user.username = email_username  
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
+     
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,16 @@
+import random
 from api.serializers import MyTokenObtainPairSerializer, CustomUserSerializer, ProfileSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from userauths.models import CustomUser, Profile
+from rest_framework import generics
+from api.serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny 
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
