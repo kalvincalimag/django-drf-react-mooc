fe9fbfc3129ea4150a7cc7968863886fb8efbfd2
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2025-02-11 12:21:22 +0800

    define urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
     
     # Core Endpoints
     path("course/category/", api_views.CategoryListAPIView.as_view()),
+    path("course/course-list/", api_views.CourseListAPIView.as_view()),
+    path("course/course-detail/<slug>/", api_views.CourseDetailAPIView.as_view()),
+    path("course/cart/", api_views.CartAPIView.as_view()),
+    path("course/cart-list/<cart_id>", api_views.CartListAPIView.as_view()),
+    path("course/cart-item-delete/<cart_id>/<item_id>/", api_views.CartItemDeleteAPIView.as_view()),
+    path("course/stats/<cart_id>/", api_views.CartStatsAPIView.as_view()),
+    path("order/create-order/", api_views.CreateOrderAPIView.as_view()),
+    path("order/coupon/", api_views.CouponApplyAPIView.as_view())
 ]
