96f90cf29c0dfd6e9060bb4607f5c786b4239a53
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2025-02-17 23:18:17 +0800

    Omit comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,8 +21,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJ_SECRET_KEY')
@@ -130,9 +128,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'templates'
@@ -153,9 +148,6 @@ ANYMAIL = {
 
 FROM_EMAIL = os.getenv("FROM_EMAIL")
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
