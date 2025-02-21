e2ace094846cdd81bcd2810134ef47d07abaa616
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2025-02-21 23:19:26 +0800

    minor ui registration tweaks

--- frontend/src/views/auth/Register.jsx
+++ frontend/src/views/auth/Register.jsx
@@ -1,47 +1,42 @@
-import React from 'react'
-import {useState, useEffect} from "react";
-import {register} from "../../utils/auth"
-import apiInstance from "../../utils/axios"
-import BaseHeader from '../partials/BaseHeader'
-import BaseFooter from '../partials/BaseFooter'
-import { Link, useNavigate } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { register } from '../../utils/auth';
+import BaseHeader from '../partials/BaseHeader';
+import BaseFooter from '../partials/BaseFooter';
 
 function Register() {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setIsLoading(true);
 
-    const {error} = await register(fullName, email, password, password2);
+    const { error } = await register(fullName, email, password, password2);
     if (error) {
       setIsLoading(false);
-      alert(error)
+      alert(error);
     } else {
       setIsLoading(false);
-      navigate("/");
-      alert("Registration successful, you have now been logged in.")
+      navigate('/');
+      alert('Registration successful, you have now been logged in.');
     }
-  } 
+  };
 
   return (
     <>
       <BaseHeader />
-
-      <section className="container d-flex flex-column vh-100" style={{ marginTop: "150px" }}>
+      <section className="container d-flex flex-column vh-100" style={{ marginTop: '150px' }}>
         <div className="row align-items-center justify-content-center g-0 h-lg-100 py-8">
           <div className="col-lg-5 col-md-8 py-8 py-xl-0">
-            <div className="card shadow">
-              <div className="card-body p-6">
+            <div className="card shadow-sm">
+              <div className="card-body p-5">
                 <div className="mb-4">
-                  <h1 className="mb-1 fw-bold">Sign up</h1>
+                  <h1 className="mb-3 fw-bold">Sign Up</h1>
                   <span>
                     Already have an account?
                     <Link to="/login/" className="ms-1">
@@ -49,71 +44,73 @@ function Register() {
                     </Link>
                   </span>
                 </div>
-                {/* Form */}
-                <form className="needs-validation" noValidate="" onSubmit={handleSubmit}>
-                  {/* Username */}
+                <form className="needs-validation" noValidate onSubmit={handleSubmit}>
                   <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Full Name</label>
+                    <label htmlFor="full_name" className="form-label">
+                      Full Name
+                    </label>
                     <input
                       type="text"
                       id="full_name"
                       className="form-control"
                       name="full_name"
                       placeholder="John Doe"
-                      required=""
+                      required
                       onChange={(e) => setFullName(e.target.value)}
                     />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email Address</label>
+                    <label htmlFor="email" className="form-label">
+                      Email Address
+                    </label>
                     <input
                       type="email"
                       id="email"
                       className="form-control"
                       name="email"
                       placeholder="johndoe@gmail.com"
-                      required=""
+                      required
                       onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
-                  
-                  {/* Password */}
                   <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
                     <input
                       type="password"
                       id="password"
                       className="form-control"
                       name="password"
                       placeholder="**************"
-                      required=""
+                      required
                       onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Confirm Password</label>
+                    <label htmlFor="password2" className="form-label">
+                      Confirm Password
+                    </label>
                     <input
                       type="password"
                       id="password2"
                       className="form-control"
                       name="password2"
                       placeholder="**************"
-                      required=""
+                      required
                       onChange={(e) => setPassword2(e.target.value)}
                     />
                   </div>
-                  <div>
-                    <div className="d-grid">
-                      {isLoading === true ? (
+                  <div className="d-grid">
+                    {isLoading ? (
                       <button disabled type="submit" className="btn btn-primary">
-                        Processing <i className='fas fa-spinner fa-spin'></i>
+                        Processing <i className="fas fa-spinner fa-spin"></i>
                       </button>
-                      ) : (
+                    ) : (
                       <button type="submit" className="btn btn-primary">
-                        Sign Up <i className='fas fa-user-plus'></i>
+                        Sign Up <i className="fas fa-user-plus"></i>
                       </button>
-                      )}
-                    </div>
+                    )}
                   </div>
                 </form>
               </div>
@@ -121,10 +118,9 @@ function Register() {
           </div>
         </div>
       </section>
-
       <BaseFooter />
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
