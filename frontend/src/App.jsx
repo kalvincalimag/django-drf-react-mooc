f29c753cf190b42834f2ebb3728c67183809757d
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2025-03-15 15:44:02 +0800

    minor BE tweaks and FE index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,23 @@ import Login from '../src/views/auth/Login';
 import Logout from './views/auth/Logout';
 import ForgotPassword from './views/auth/ForgotPassword';
 import CreateNewPassword from './views/auth/CreateNewPassword';
+import Index from './views/base/Index';
 
 function App() {
   return (
     <BrowserRouter>
       <MainWrapper>
         <Routes>
+
+          {/* Authentication */}
           <Route path='/register/' element={<Register/>}/>
           <Route path='/login/' element={<Login/>}/>
           <Route path='/logout/' element={<Logout/>}/>
           <Route path='/forgot-password/' element={<ForgotPassword/>}/>
           <Route path='/create-new-password/' element={<CreateNewPassword/>}/>
+
+          {/* Core Routes */}
+          <Route path='' element={<Index/>}/>
         </Routes>
       </MainWrapper>
     </BrowserRouter>
